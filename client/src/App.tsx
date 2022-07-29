ecc890ed677999cb8047db8fa25838b167c7e71f
Tyler Brown <tylershaynebrown@gmail.com>
2022-07-28 20:32:06 -0400

    chore: set up client with vite
    * Created new react-ts vite project.
    
    * Installed eslint/prettier.
    
    * Installed Tailwind.
    
    * Removed fluff from new project.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
+const App = () => {
+  const testing = "testing"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full border-4 border-red-400 bg-black">
+      <h1 className="text-center text-red-500">Hello Worlsdfsdfd</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
